12e01480a3f90c6b674466a1cca1925b296f00ca
kenansezginbas <kenansezginbas@hotmail.com>
2023-01-02 23:56:10 +0300

    Dart Backend

---
+++ bin/dart_bk.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+void main(List<String> arguments) {
+  final app = Router();
+  final Map<String, dynamic> userData = {
+    "username": "Key",
+    "fullname": "Kenan Sezginbas",
+    "age": 31,
+    "x": true,
+  };
+
+  app.get("/", (Request request) {
+    return Response.ok(jsonEncode(userData));
+  });
+
+  app.post("/", (Request request) {
+    return Response.ok("Post Islemi");
+  });
+
+  serve(app, "localhost", 8080);
+}
---
+++ test/dart_bk_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_bk/dart_bk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
